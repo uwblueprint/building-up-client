40071d11265234542ad841b912b14f25cfe9aa8b
Faizaan Madhani <fzmadhani@gmail.com>
2020-09-27 20:06:09 -0400

    Boilerplate (#6)
    * Add Apollo Client
    
    * Set up React Router and Redux -- Incomplete Store Page
    
    * Fix newline

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class StoreFront extends Component {
+    constructor(props) {
+        super()
+        console.log(this.props)
+    }
+
+    render () {
+        return (
+            <h1>Building Up -- Home Page</h1>
+        )
+    }
+}
+
+export default StoreFront
---
+++ src/pages/storefront.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class StoreFront extends Component {
+    constructor(props) {
+        super()
+        console.log(this.props)
+    }
+
+    render () {
+        return (
+            <h1>This is the store page</h1>
+        )
+    }
+}
+
+export default StoreFront
+
