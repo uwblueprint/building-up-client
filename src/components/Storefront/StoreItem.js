fde47625257fb78c1483cc7976beb5e06f7a6622
Samantha Orizabal <sam.orizabal@gmail.com>
2020-11-01 21:27:13 -0500

    add onAddCartClick and onMoreDetailsClick to StoreItemDialog

--- src/components/Storefront/StoreItem.js
+++ src/components/Storefront/StoreItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Box } from '@material-ui/core';
+// import { useShopify } from '../../data/reducers/index';
 
 const useStyles = makeStyles(() => ({
   description: {
--- src/components/Storefront/StoreItemDialog.js
+++ src/components/Storefront/StoreItemDialog.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Box, Dialog, Button, IconButton } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
+import { useShopify } from '../../hooks/useShopify';
+// import  { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles(() => ({
   description: {
@@ -22,10 +24,40 @@ const useStyles = makeStyles(() => ({
 
 const StoreItemDialog = ({ itemName, price, onClose, open }) => {
   const classes = useStyles();
+  // let history = useHistory();
+
+  const {
+    openCart,
+    checkoutState,
+    addVariant,
+    fetchProduct,
+  } = useShopify();
 
   const onAddCartClick = () => {
     console.log('Add to Cart clicked');
+
+
+    openCart();
+    const checkoutId = checkoutState.id;
+
+    const lineItemsToAdd = [
+      { colour: 'black', quantity: 1 }
+    ];
+
+    addVariant(checkoutId, lineItemsToAdd);
   };
+
+  const onMoreDetailsClick = (event, productId) => {
+    console.log('See more details clicked');
+
+    event.preventDefault();
+    const id = productId;
+    fetchProduct(id).then((res) => {
+      // Redirect to product page ?
+      // history.push(`/StoreItem/${res.id}`);
+      console.log(res);
+    })
+  }
 
   return (
     <Dialog onClose={onClose} open={open}>
@@ -57,6 +89,7 @@ const StoreItemDialog = ({ itemName, price, onClose, open }) => {
               <Typography variant="body2" color="textSecondary" component="p">
                 ${price.toFixed(2)}
               </Typography>
+              <a onClick={onMoreDetailsClick} >See more details</a>
               <Button
                 variant="outlined"
                 onClick={onAddCartClick}
