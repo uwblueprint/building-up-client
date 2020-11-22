ad06254e268b23099262dedac46cc2452af5ab65
Ellen Choi <ellenchoi98@outlook.com>
2020-11-22 22:13:50 +0900

    Added polling for itemsSold

--- src/pages/TeamDashboard/teamdashboard.js
+++ src/pages/TeamDashboard/teamdashboard.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react'
 import { useQuery, gql } from '@apollo/client'
 
+// TODO: separate fields that require polling from this query. Right now, polling is applied to all fields
 const GET_TEAM_INFO = gql`
 query getTeam($id: Int!) {
   getTeam(id: $id) {
     name
     organization
+    toquesSold
+    capsSold
     id
   }
 }
@@ -14,17 +17,19 @@ query getTeam($id: Int!) {
 const TeamDashboard = (props) => {
   const id = Number(props.match.params.id)
   const { loading, error, data } = useQuery(GET_TEAM_INFO, {
-    variables: { id }
-  })
-  console.log("This is the team data: ", data)
+    variables: { id },
+    pollInterval: 500
+  });
+  console.log("This is the team data: ", data);
   if (loading) return 'Loading...'
-  if (error) return `Error! ${error.message}`
+  if (error) return `Error!2 ${error.message}`
   return (
     <Fragment>
       <h1>This is the Team Dashboard page</h1>
       <h2>The Team Using this has ID {id}</h2>
       <h2>This is their data: {JSON.stringify(data.getTeam)}</h2>
     </Fragment>
+    
   )
 }
 
