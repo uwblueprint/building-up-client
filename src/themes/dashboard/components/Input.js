670b162b1b186fe0a1ee3460590e9f107ddbbe8e
Jeffrey Huang <jeff.huang62@gmail.com>
2021-02-15 18:23:14 -0500

    Input field element for dashboard according to design system

---
+++ src/themes/dashboard/components/Input.js
@@ -0,0 +1,21 @@
+const Input = {
+  parts: ['field'],
+  baseStyle: {
+    field: {
+      p: '12px 16px',
+    },
+  },
+  sizes: {
+    md: {
+      field: {
+        height: '48px',
+        borderRadius: '4px',
+      },
+    },
+  },
+  defaultProps: {
+    focusBorderColor: '#F5D5D5',
+  },
+};
+
+export default Input;
