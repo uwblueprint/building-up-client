d541efd1ba8d1999aac061783f4400d1eabaa5f5
Jeffrey Huang <jeff.huang62@gmail.com>
2021-02-15 17:20:43 -0500

    run prettier

--- src/data/actions/auth.js
+++ src/data/actions/auth.js
@@ -1,121 +1,114 @@
-import {
-    REGISTER_SUCCESS,
-    REGISTER_FAIL,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    LOGOUT_FAIL,
-    LOGOUT_SUCCESS,
-} from "./type";
+import { REGISTER_SUCCESS, REGISTER_FAIL, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_FAIL, LOGOUT_SUCCESS } from './type';
 
-import AuthService from "../services/auth.service";
+import AuthService from '../services/auth.service';
 
-export const register = (firstName, lastName, email, password, client) => (dispatch) => {
+export const register = (firstName, lastName, email, password, client) => dispatch => {
   return AuthService.register(firstName, lastName, email, password, client).then(
-    (response) => {
+    response => {
       console.log(response);
       dispatch({
         type: REGISTER_SUCCESS,
-      });  
+      });
       return Promise.resolve();
     },
-    (error) => {
+    error => {
       console.log(error);
       dispatch({
         type: REGISTER_FAIL,
       });
       return Promise.reject();
-    }
+    },
   );
 };
 
-export const login = (email, password, client) => (dispatch) => {
+export const login = (email, password, client) => dispatch => {
   return AuthService.login(email, password, client).then(
-    (res) => {
+    res => {
       //login successful
-      if (res.data.login !== null){
-        const { firstName, lastName, email, id, teamId } = res.data.login
+      if (res.data.login !== null) {
+        const { firstName, lastName, email, id, teamId } = res.data.login;
         dispatch({
           type: LOGIN_SUCCESS,
-          payload: { 
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
+          payload: {
+            firstName,
+            lastName,
+            email,
             userId: id,
-            teamId: teamId,
+            teamId,
           },
         });
-      //login failed
-      }else{
+        //login failed
+      } else {
         dispatch({
           type: LOGIN_FAIL,
         });
       }
       return Promise.resolve();
     },
-    (error) => {
+    error => {
       console.log(error);
       dispatch({
         type: LOGIN_FAIL,
       });
       return Promise.reject();
-    }
+    },
   );
 };
 
-export const logout = (client) => (dispatch) => {
+export const logout = client => dispatch => {
   return AuthService.logout(client).then(
-    (res) => {
-      if (res !== null){ 
+    res => {
+      if (res !== null) {
         dispatch({
           type: LOGOUT_SUCCESS,
         });
-      }else{
+      } else {
         dispatch({
           type: LOGOUT_FAIL,
         });
       }
       return Promise.resolve();
     },
-    (error) => {
+    error => {
       console.error(error);
       dispatch({
         type: LOGOUT_FAIL,
       });
       return Promise.reject();
-    }
+    },
   );
 };
 
-export const currentUser = (client) => (dispatch) => {
+export const currentUser = client => dispatch => {
   return AuthService.getCurrentUser(client).then(
-    (res) => {
+    res => {
       //login successful
-      if (res.data.getActiveUser !== null){
-        const { firstName, lastName, email, id, teamId } = res.data.getActiveUser
+      if (res.data.getActiveUser !== null) {
+        const { firstName, lastName, email, id, teamId } = res.data.getActiveUser;
         dispatch({
           type: LOGIN_SUCCESS,
-          payload: { 
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
+          payload: {
+            firstName,
+            lastName,
+            email,
             userId: id,
-            teamId: teamId,
+            teamId,
           },
         });
-      //login failed
-      }else{
+        //login failed
+      } else {
         dispatch({
           type: LOGIN_FAIL,
         });
       }
       return Promise.resolve();
     },
-    (error) => { 
+    error => {
       console.error(error);
       dispatch({
         type: LOGIN_FAIL,
       });
       return Promise.reject();
-    }
+    },
   );
 };
