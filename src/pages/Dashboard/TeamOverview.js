16f586dd4e49a569a3c78506c7968d12dbfb8c06
Hyunzee Kim <hyunzeekim01@gmail.com>
2021-02-28 16:11:28 -0500

    address PR comments

--- src/pages/Dashboard/TeamOverview.js
+++ src/pages/Dashboard/TeamOverview.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
-import { Box, Button, Center, Spacer, Heading } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, Heading, Spacer } from '@chakra-ui/react';
 
 const GET_TEAM_INFO = gql`
   query getTeam($id: Int!) {
@@ -21,10 +21,10 @@ const StorefrontButton = () => {
 const SalesInfo = props => {
   return (
     <Box mr="100px">
-      <Heading fontSize="h4" color="gray.400" mb="8px">
+      <Heading textTransform="uppercase" size="subtitle" color="gray.500" mb="8px">
         {props.description}
       </Heading>
-      <Heading fontSize="32px">{props.amount}</Heading>
+      <Heading size="h2">{props.amount}</Heading>
     </Box>
   );
 };
@@ -33,7 +33,7 @@ const NoSales = () => {
   return (
     <Box w="100%" h="300px" bg="background.primary">
       <Center w="100%">
-        <Heading fontSize="20px" mt="69px" mb="24px">
+        <Heading size="h4" mt="69px" mb="24px">
           You don’t have sales yet. Share your storelink to get started!
         </Heading>
       </Center>
@@ -55,22 +55,22 @@ const TeamOverview = ({ teamId }) => {
     `Error! ${error.message}`
   ) : (
     <Box w="100%">
-      <Heading fontSize="h4" color="gray.400" mb="8px">
+      <Heading textTransform="uppercase" size="subtitle" color="gray.500" mb="8px">
         Team {data.getTeam.name}
       </Heading>
-      <Box d="flex" mb="40px">
-        <Heading fontSize="36px">Dashboard</Heading>
+      <Flex mb="40px">
+        <Heading size="h1">Dashboard</Heading>
         <Spacer />
         {data.getTeam.itemsSold !== 0 ? <StorefrontButton /> : null}
-      </Box>
-      <Heading fontSize="24px" mb="23px">
+      </Flex>
+      <Heading size="h3" mb="23px">
         Overview
       </Heading>
-      <Box d="flex" mb="72px">
-        <SalesInfo description="TOTAL ITEMS SOLD" amount={data.getTeam.itemsSold} />
-        <SalesInfo description="TOTAL CAPITAL RAISED" amount={'$' + data.getTeam.amountRaised} />
-      </Box>
-      <Heading fontSize="24px" mb="21px">
+      <Flex mb="72px">
+        <SalesInfo description="Total Items Sold" amount={data.getTeam.itemsSold} />
+        <SalesInfo description="Total Capital Raised" amount={'$' + data.getTeam.amountRaised} />
+      </Flex>
+      <Heading size="h3" mb="21px">
         Sales Log
       </Heading>
       {data.getTeam.itemsSold === 0 ? <NoSales /> : null}
