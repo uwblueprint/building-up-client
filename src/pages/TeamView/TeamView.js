93a78ce4c28eaf90082095c4ad11db10d7119edc
Cynthia Ding <cy2ding@uwaterloo.ca>
2021-03-04 18:37:46 -0500

    Making changes according to PR comments: prettify, id as string, adding TODO comments, changing styling to use heading types, adding a key field to return value of form inputs

--- src/pages/TeamView/TeamView.js
+++ src/pages/TeamView/TeamView.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import { useSelector } from 'react-redux';
-import { Box, Button, Heading, Input, FormControl} from '@chakra-ui/react';
+import { Box, Button, Heading, Input, FormControl } from '@chakra-ui/react';
 
 const GET_TEAM_INFO = gql`
-  query getTeam($id: Int!) {
+  query getTeam($id: String!) {
     getTeam(id: $id) {
       name
       organization
@@ -31,68 +31,76 @@ const TeamView = () => {
 };
 
 const InviteTeamMembers = () => {
-  const [inputList, setInputList] = useState([{email: ""}]);
+  const [inputList, setInputList] = useState([{ email: '' }]);
 
   const handleInputChange = (e, index) => {
-    const { name, value} = e.target;
+    const { name, value } = e.target;
     const list = [...inputList];
     list[index][name] = value;
     setInputList(list);
-  }
+  };
 
   const handleRemoveClick = index => {
     const list = [...inputList];
     list.splice(index, 1);
     setInputList(list);
-  }
+  };
 
   const handleAddClick = () => {
-    setInputList([...inputList, {email: ""}])
-  }
+    setInputList([...inputList, { email: '' }]);
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
+    //TODO: The below displays the inputted emails as an alert
+    //      Should change to actually handle the submit function
     const list = [...inputList];
-    let output = "";
+    let output = '';
     for (let i = 0; i < list.length; i++) {
       output = output.concat(list[i].email);
     }
     alert(output);
-  }
+  };
 
   return (
     <Box w="50%">
       <form onSubmit={e => handleSubmit(e)}>
         {inputList.map((x, i) => {
           return (
-            <Box d="flex" mb="8px">
-              <FormControl w = "80%">
-                <Input type="email" name="email" placeholder="Email" value={x.email} onChange={e => handleInputChange(e, i)}/>
+            <Box key={i} d="flex" mb="8px">
+              <FormControl w="80%">
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={x.email}
+                  onChange={e => handleInputChange(e, i)}
+                />
               </FormControl>
-              {inputList.length !== 1 ? <Button variant="ghost" onClick={() => handleRemoveClick(i)} maxW = "15%"> x </Button> : null}
+              {inputList.length !== 1 ? (
+                <Button variant="ghost" onClick={() => handleRemoveClick(i)} maxW="15%">
+                  x
+                </Button>
+              ) : null}
             </Box>
           );
         })}
-        <Button variant="link" mb="40px" fontSize = "16px" onClick={handleAddClick} display="block"> 
+        <Button variant="link" mb="40px" fontSize="16px" onClick={handleAddClick} display="block">
           + Add Another
         </Button>
-        <Button type = "submit" display="block">
+        <Button type="submit" display="block">
           Send Invites
         </Button>
       </form>
     </Box>
   );
-
 };
 
 const TeamMembers = () => {
-    return (
-      <Box w="100%" h="500px" mb = "64px" background = "gray.400">
-        
-      </Box>
-    );
-  };
-  
+  //TODO: Implement the team members table using the shared table component
+  return <Box w="100%" h="500px" mb="64px" background="gray.400"></Box>;
+};
+
 const TeamOverview = ({ teamId }) => {
   const { loading, error, data } = useQuery(GET_TEAM_INFO, {
     variables: { id: teamId },
@@ -104,21 +112,21 @@ const TeamOverview = ({ teamId }) => {
     `Error! ${error.message}`
   ) : (
     <Box w="100%">
-      <Heading fontSize="16px" color="gray.400" mb="8px">
-      Team {data.getTeam.name}
-    </Heading>
-    <Heading fontSize="36px" mb = "24px">
-      Team Members
-    </Heading>
-    <TeamMembers />
-    <Heading fontSize="24px" mb="8px">
-      Invite Team Members
-    </Heading>
-    <Heading fontSize="16px" fontWeight="normal" mb="24px">
-      Enter the emails of the people you want to add
-    </Heading>
-    <InviteTeamMembers />
-  </Box>
+      <Heading textTransform="uppercase" as="p" size="subtitle" color="gray.500" mb="8px">
+        Team {data.getTeam.name}
+      </Heading>
+      <Heading as="h1" size="h1" mb="24px">
+        Team Members
+      </Heading>
+      <TeamMembers />
+      <Heading as="h3" size="h3" mb="8px">
+        Invite Team Members
+      </Heading>
+      <Heading as="p" size="normal" fontWeight="normal" mb="24px">
+        Enter the emails of the people you want to add
+      </Heading>
+      <InviteTeamMembers />
+    </Box>
   );
 };
 
