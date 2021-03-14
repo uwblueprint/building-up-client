32f44b0fe2284564f723b66f79f27d434de6181b
kevinz999@gmail.com <kevinz999@gmail.com>
2021-03-14 16:13:51 -0400

    made  pr comment changes

--- src/components/Storefront/Footer.js
+++ src/components/Storefront/Footer.js
@@ -1,99 +1,84 @@
 import React, { useState } from 'react';
-import { Image, Center, Text, Link, Button, HStack, VStack, Input } from '@chakra-ui/react';
-import { ChevronRightIcon } from '@chakra-ui/icons';
-import { FaFacebookSquare, FaInstagramSquare, FaYoutube, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { Heading, Image, Box, Center, Text, Link, Button, HStack, VStack, Icon, Input } from '@chakra-ui/react';
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaYoutube,
+  FaLinkedin,
+  FaTwitter,
+  FaChevronCircleRight,
+} from 'react-icons/fa';
 import smallLogo from '../../assets/building_up_footer_small_icon.png';
 
-const Footer = props => {
-  const { subscribtionEmail, changeSubscriptionEmail } = useState();
+const Footer = () => {
+  const [subscribtionEmail, setSubscribtionEmail] = useState('');
 
-  const subscribe = e => {
+  const handleSubscriptionEmailChange = e => {
     e.preventDefault();
-    changeSubscriptionEmail(e);
+    setSubscribtionEmail(e.target.value);
+    console.log(subscribtionEmail);
   };
 
-  const updateSubscriptionEmail = e => {
+  const handleSubscriptionEmailSubmit = e => {
     e.preventDefault();
     alert(subscribtionEmail);
   };
 
   return (
-    <VStack spacing="0px">
-      <HStack backgroundColor="black" w="100%" display="flex" paddingTop="54px" paddingBottom="84px">
-        <Image src={smallLogo} alt="Small Icon" marginLeft="41px" marginRight="47px" alignSelf="flex-start" />
-        <HStack backgroundColor="black" w="100%" display="flex" justifyContent="space-between">
-          <VStack w="13%" alignItems="flex-start" spacing="26px">
-            <Text fontWeight="extrabold" letterSpacing="wider" color="white">
+    <Box color="white">
+      <HStack backgroundColor="black" paddingLeft="41px" paddingTop="54px" paddingBottom="84px" paddingRight="105px">
+        <Image src={smallLogo} alt="Small Icon" marginRight="47px" alignSelf="flex-start" />
+        <HStack w="100%" h="100%" backgroundColor="black" justifyContent="space-between">
+          <VStack alignItems="flex-start" spacing="26px" h="100%" marginTop="0px">
+            <Text fontWeight="extrabold" letterSpacing="wider" color="white" fontFamily="Jost">
               GET IN TOUCH
             </Text>
-            <Text color="white" fontFamily="Karla">
+            <Text>
               116 Industry Street <br /> Suite 313 <br /> Toronto, ON M6M 4L8
             </Text>
-            <Link href="mailto:info@raisingtheroof.org" color="white" fontFamily="Karla" fontWeight="normal">
-              info@raisingtheroof.org
-            </Link>
-            <HStack spacing="5">
-              <FaFacebookSquare fontSize="26px" color="white" />
-              <FaInstagramSquare fontSize="26px" color="white" />
-              <Center h="22px" w="22px" backgroundColor="white" borderRadius="2px">
+            <Link href="mailto:info@raisingtheroof.org">info@raisingtheroof.org</Link>
+            <HStack spacing="20px">
+              <FaFacebookSquare fontSize="26px" />
+              <FaInstagramSquare fontSize="26px" />
+              <Center h="22px" w="22px" backgroundColor="white" borderRadius="2px" color="black">
                 <FaYoutube />
               </Center>
               <FaLinkedin fontSize="26px" color="white" />
-              <Center h="22px" w="22px" backgroundColor="white" borderRadius="2px">
+              <Center h="22px" w="22px" backgroundColor="white" borderRadius="2px" color="black">
                 <FaTwitter />
               </Center>
             </HStack>
           </VStack>
-          <VStack
-            as="form"
-            alignSelf="flex-end"
-            w="31%"
-            alignItems="flex-start"
-            onSubmit={subscribe}
-            marginRight="105px"
-            spacing="26px"
-          >
-            <Text fontWeight="extrabold" letterSpacing="wider" color="white">
+          <VStack as="form" w="31%" alignItems="flex-start" onSubmit={handleSubscriptionEmailSubmit} spacing="26px">
+            <Text fontWeight="extrabold" letterSpacing="wider" fontFamily="Jost">
               STAY CONNECTED
             </Text>
-            <Input
-              placeholder="YOUR EMAIL ADDRESS*"
-              borderColor="#7C7C7Cd"
-              fontFamily="Karla"
-              fontSize="12px"
-              fontWeight="bold"
-              letterSpacing="wider"
-              borderRadius="0px"
-              textColor="#7C7C7C"
-              onChange={updateSubscriptionEmail}
-            ></Input>
-            <Text letterSpacing="wider" color="#7C7C7C" fontFamily="Karla" fontWeight="normal">
+            <Input placeholder="YOUR EMAIL ADDRESS*" onChange={handleSubscriptionEmailChange} />
+            <Text letterSpacing="wider" color="#7C7C7C">
               In case you want to join the list of recipients of our news and updates, please fill in your email.
             </Text>
-            <Button backgroundColor="white" borderRadius="0px" type="submit" padding="16px 35px">
-              SUBSCRIBE <ChevronRightIcon backgroundColor="black" color="white" borderRadius="100%" marginLeft="11px" />
+            <Button size="sm" variant="inverted" color="black" rightIcon={<Icon as={FaChevronCircleRight} />}>
+              <Heading size="subtitle"> SUBSCRIBE </Heading>
             </Button>
           </VStack>
         </HStack>
       </HStack>
       <HStack
         justifyContent="space-between"
-        fontFamily="Karla"
-        color="white"
         backgroundColor="#BE2A1B"
-        w="100%"
+        paddingLeft="83px"
         paddingTop="31px"
         paddingBottom="26px"
+        paddingRight="105px"
       >
-        <Text marginLeft="83px">2020 RAISING THE ROOF. ALL RIGHTS RESERVED. CHARTIABLE #139744569RR0001. </Text>
-        <HStack fontWeight="normal" spacing="48px" marginRight="105px">
-          <Link color="white">SITEMAP</Link>
-          <Link color="white">PRIVACY POLICY </Link>
-          <Link color="white">TERMS OF USE</Link>
-          <Link color="white">SITE CREDITS</Link>
+        <Text>2020 RAISING THE ROOF. ALL RIGHTS RESERVED. CHARTIABLE #139744569RR0001. </Text>
+        <HStack spacing="48px">
+          <Link>PRIVACY POLICY </Link>
+          <Link>TERMS OF USE</Link>
+          <Link href="https://uwblueprint.org/">SITE CREDITS</Link>
         </HStack>
       </HStack>
-    </VStack>
+    </Box>
   );
 };
 
