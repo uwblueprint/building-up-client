1a2a1546bc9f2e7a2ca68a92232700ee5400cd2b
Hong Yi Chen <hongyigma@gmail.com>
2021-03-28 16:23:46 -0400

    Refactored existing Cart page

--- src/components/storefront/StorefrontRouter/StorefrontRouter.js
+++ src/components/storefront/StorefrontRouter/StorefrontRouter.js
@@ -4,9 +4,10 @@ import { ChakraProvider } from '@chakra-ui/react';
 import storeTheme from 'themes/store';
 import { useShopify } from '../../../hooks/useShopify';
 import { Navbar, Footer } from '../';
-import Home from '../../../pages/Storefront/Home';
-import Product from '../../../pages/Storefront/Product';
+import Home from 'pages/Storefront/Home';
+import Product from 'pages/Storefront/Product';
 import PageNotFound from 'pages/Storefront/PageNotFound';
+import Cart from 'pages/Storefront/Cart';
 
 const StorefrontRouter = () => {
   const { createShop, createCheckout, fetchProducts, fetchCollections } = useShopify();
@@ -30,6 +31,9 @@ const StorefrontRouter = () => {
         </Route>
         <Route path={`${path}/products/:id`}>
           <Product />
+        </Route>
+        <Route path={`${path}/cart`}>
+          <Cart />
         </Route>
         <Route path={`${path}/*`}>
           <PageNotFound />
---
+++ src/pages/Storefront/Cart.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
+import { selectors as teamSelectors } from '../../data/reducers/team';
+import { useShopify } from '../../hooks/useShopify';
+
+// Cart page that shows the user the items in their cart, allows them to change quantity, and proceed to checkout
+
+const Cart = () => {
+  const { checkoutState, updateCartAttributes } = useShopify();
+  const team = useSelector(teamSelectors.selectTeam, shallowEqual);
+
+  //   useEffect(() => {
+  //     const userID = sessionStorage.getItem('userID');
+  //     updateCartAttributes(checkoutState.id, [
+  //       { key: 'userID', value: userID ? userID.toString() : '1' }, // Temporary, so that page doesn't crash
+  //       // Remove once user info is added to redux
+  //       { key: 'teamID', value: team.id.toString() },
+  //       { key: 'teamName', value: team.name },
+  //     ]);
+  //   }, [checkoutState.id, team.id, team.name]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  //   const openCheckout = () => {
+  //     if (checkoutState.webUrl) {
+  //       console.log('webUrl exists');
+  //     }
+  //     // window.open(checkoutState.webUrl) // opens checkout in a new window
+  //     window.location.replace(checkoutState.webUrl); // opens checkout in same window
+  //   };
+
+  return (
+    <div>
+      {/* <Box display="flex" flexDirection="column" alignItems="center">
+  <Typography variant="h2" className={classes.cartHeading}>
+    Cart
+  </Typography>
+  <Box display="flex" flexDirection="column" alignItems="center" flexGrow={1}>
+    {checkoutState.lineItems && checkoutState.lineItems.map((lineItem, i) => <CartItem key={i} lineItem={lineItem} />)}
+  </Box>
+  <Button variant="contained" color="primary" onClick={openCheckout} className={classes.checkoutButton}>
+    Proceed to Checkout
+  </Button>
+</Box>; */}
+    </div>
+  );
+};
+
+export default Cart;
