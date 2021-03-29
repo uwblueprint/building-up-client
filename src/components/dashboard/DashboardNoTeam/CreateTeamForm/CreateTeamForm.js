633290369a5ab71cb57a33dd964f0a071f63a183
Jeffrey Huang <jeff.huang62@gmail.com>
2021-03-29 16:03:27 -0400

    Abstract invite team members form

--- src/components/dashboard/DashboardNoTeam/CreateTeamForm/CreateTeamForm.js
+++ src/components/dashboard/DashboardNoTeam/CreateTeamForm/CreateTeamForm.js
@@ -1,56 +1,7 @@
 import React from 'react';
 import { Box, Text, Button, Input, Heading, HStack, VStack, Flex, FormControl } from '@chakra-ui/react';
 
-const InviteTeamMembers = props => {
-  const { inputList, setInputList } = props;
-  const handleInputChange = (e, index) => {
-    const { value } = e.target;
-    const list = [...inputList];
-    list[index] = value;
-    setInputList(list);
-  };
-
-  const handleRemoveClick = index => {
-    const list = [...inputList];
-    list.splice(index, 1);
-    setInputList(list);
-  };
-
-  const handleAddClick = () => {
-    setInputList([...inputList, '']);
-  };
-
-  return (
-    <Box w="100%">
-      <VStack align="flex-start">
-        {inputList.map((x, i) => {
-          return (
-            <Flex key={i} mb="8px" w="100%">
-              <FormControl w="80%">
-                <Input
-                  bg="white"
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  value={x}
-                  onChange={e => handleInputChange(e, i)}
-                />
-              </FormControl>
-              {inputList.length !== 1 ? (
-                <Button variant="ghost" onClick={() => handleRemoveClick(i)} maxW="15%">
-                  x
-                </Button>
-              ) : null}
-            </Flex>
-          );
-        })}
-        <Button variant="link" mb="40px" fontSize="16px" onClick={handleAddClick}>
-          + Add Another
-        </Button>
-      </VStack>
-    </Box>
-  );
-};
+import InviteTeamMembers from 'components/dashboard/InviteTeamMembers/InviteTeamMembers';
 
 const CreateTeamForm = props => {
   const {
--- src/pages/TeamOverview/TeamOverview.js
+++ src/pages/TeamOverview/TeamOverview.js
@@ -2,100 +2,60 @@ import React, { useEffect, useState } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
 import { useSelector } from 'react-redux';
 
-import { Box, Button, Heading, Input, FormControl, Flex, VStack, Text } from '@chakra-ui/react';
+import { Box, Heading, Text, useToast } from '@chakra-ui/react';
 
 import { GET_USERS_FOR_TEAM, SEND_INVITE_EMAILS } from 'data/gql/team';
 import { LEAVE_TEAM } from 'data/gql/user';
 
+import InviteTeamMembers from 'components/dashboard/InviteTeamMembers/InviteTeamMembers';
 import PageHeading from 'components/dashboard/PageHeading/PageHeading';
 import TeamMembersTable from 'components/dashboard/TeamMembersTable/TeamMembersTable';
+import Toast from 'components/dashboard/Toast/Toast';
 
-const InviteTeamMembers = () => {
-  const [inputList, setInputList] = useState(['']);
-
-  const handleInputChange = (e, index) => {
-    const { value } = e.target;
-    const list = [...inputList];
-    list[index] = value;
-    setInputList(list);
-  };
-
-  const handleRemoveClick = index => {
-    const list = [...inputList];
-    list.splice(index, 1);
-    setInputList(list);
-  };
-
-  const handleAddClick = () => {
-    setInputList([...inputList, '']);
-  };
-
-  const [inviteUsersToTeam] = useMutation(SEND_INVITE_EMAILS);
+const TeamOverview = ({ team }) => {
+  const [inviteEmails, setInviteEmails] = useState(['']);
   const {
     user: { teamId },
   } = useSelector(state => state.auth);
+  const toast = useToast();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const list = [...inputList];
-    inviteUsersToTeam({
-      variables: { emails: list, teamId: teamId },
-    })
-      .then(data => {
-        console.log(data);
-        // Create a toast or alert to indicate emails have been sent
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
-
-  return (
-    <Box w="50%">
-      <form onSubmit={e => handleSubmit(e)}>
-        <VStack align="flex-start">
-          {inputList.map((x, i) => {
-            return (
-              <Flex key={i} mb="8px" w="100%">
-                <FormControl w="80%">
-                  <Input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={x}
-                    onChange={e => handleInputChange(e, i)}
-                  />
-                </FormControl>
-                {inputList.length !== 1 ? (
-                  <Button variant="ghost" onClick={() => handleRemoveClick(i)} maxW="15%">
-                    x
-                  </Button>
-                ) : null}
-              </Flex>
-            );
-          })}
-          <Button variant="link" mb="24px" fontSize="16px" onClick={handleAddClick}>
-            + Add Another
-          </Button>
-          <Button size="lg" type="submit">
-            Send Invites
-          </Button>
-        </VStack>
-      </form>
-    </Box>
-  );
-};
-
-const TeamOverview = ({ team }) => {
   const { loading: loadingMembers, error: errorMembers, data: members, updateQuery } = useQuery(GET_USERS_FOR_TEAM, {
     variables: { teamId: team.teamId },
   });
 
   const [leaveTeam, { loading: loadingRemove, data: leaveTeamData }] = useMutation(LEAVE_TEAM);
-
   const handleRemove = id => {
     leaveTeam({ variables: { id } });
   };
+
+  const [inviteUsersToTeam, { loading: loadingInvite, error, data }] = useMutation(SEND_INVITE_EMAILS);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    inviteUsersToTeam({
+      variables: { emails: inviteEmails, teamId: teamId },
+    });
+  };
+
+  useEffect(() => {
+    if (data) {
+      toast({
+        position: 'top',
+        render: props => <Toast {...props} description="Email invites sent!" isClosable />,
+      });
+      setInviteEmails(['']);
+    }
+  }, [data, toast]);
+
+  useEffect(() => {
+    if (error) {
+      toast({
+        title: 'Error sending email invites.',
+        status: 'error',
+        isClosable: true,
+      });
+    }
+  }, [error, toast]);
 
   useEffect(() => {
     if (leaveTeamData) {
@@ -131,7 +91,16 @@ const TeamOverview = ({ team }) => {
         <Text size="normal" mb="24px">
           Enter the emails of the people you want to add
         </Text>
-        <InviteTeamMembers />
+        <Box w="50%">
+          <InviteTeamMembers
+            inputList={inviteEmails}
+            setInputList={setInviteEmails}
+            handleSubmit={handleSubmit}
+            isEmailRequired
+            withSubmitButton
+            isSubmitLoading={loadingInvite}
+          />
+        </Box>
       </Box>
     </>
   );
