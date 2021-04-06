bf5c275cb59b4db122a2b68f4111a60065388b2c
Hong Yi Chen <hongyigma@gmail.com>
2021-04-05 21:36:53 -0400

    Documentation + stylings

--- src/pages/Storefront/Cart.js
+++ src/pages/Storefront/Cart.js
@@ -2,27 +2,26 @@ import React, { useEffect } from 'react';
 import { useShopify } from '../../hooks/useShopify';
 import { CartItem } from 'components/storefront';
 import { Box, HStack, VStack, Heading, Divider, Flex, FormControl, Text, Button, Input } from '@chakra-ui/react';
-
 import { useSelector, shallowEqual } from 'react-redux';
 import { selectors as teamSelectors } from '../../data/reducers/team';
 
 // Cart page that shows the user the items in their cart, allows them to change quantity, and proceed to checkout
 
 const Cart = () => {
-  // you're going to need to update products so that it's only the ones inside their cart
   const { products, checkoutState, updateCartAttributes } = useShopify();
-  // checkoutState is currently empty, you're going to need to add to it later on
   const team = useSelector(teamSelectors.selectTeam, shallowEqual);
 
-  //   useEffect(() => {
-  //     const userID = sessionStorage.getItem('userID');
-  //     updateCartAttributes(checkoutState.id, [
-  //       { key: 'userID', value: userID ? userID.toString() : '1' }, // Temporary, so that page doesn't crash
-  //       // Remove once user info is added to redux
-  //       { key: 'teamID', value: team.id.toString() },
-  //       { key: 'teamName', value: team.name },
-  //     ]);
-  //   }, [checkoutState.id, team.id, team.name]); // eslint-disable-line react-hooks/exhaustive-deps
+  /* previous code that might be userful
+  useEffect(() => {
+    const userID = sessionStorage.getItem('userID');
+    updateCartAttributes(checkoutState.id, [
+      { key: 'userID', value: userID ? userID.toString() : '1' }, // Temporary, so that page doesn't crash
+      // Remove once user info is added to redux
+      { key: 'teamID', value: team.id.toString() },
+      { key: 'teamName', value: team.name },
+    ]);
+  }, [checkoutState.id, team.id, team.name]); // eslint-disable-line react-hooks/exhaustive-deps
+  */
 
   // Temporary checkout code from last term, will need to udpate
   const openCheckout = () => {
@@ -34,32 +33,29 @@ const Cart = () => {
   };
 
   const applyCoupon = () => {
-    console.log('coupon');
+    alert('To be implemented');
   };
 
   return (
     <>
-      <Box bg="black" w="100%" color="black" px="105px">
-        <Heading alignSelf="flex-start" color="white" py={5}>
+      <Box bg="black" color="black" px="105px">
+        <Heading alignSelf="flex-start" color="white" py={6}>
           MY SHOPPING BAG
         </Heading>
       </Box>
       <HStack w="100%" h="100%" justifyContent="space-between" alignItems="flex-start" px="105px">
-        <VStack w="100%" alignItems="flex-start" pt="64px" pb="64px" pr="45px">
+        <VStack w="100%" alignItems="flex-start" py="64px" pr="45px">
           <Flex w="100%" justifyContent="space-between">
             <Heading as="h4" size="subtitle" color="brand.black">
               CONTINUE SHOPPING
             </Heading>
-            <Heading as="h4" size="subtitle" textTransform="uppercase" color="brand.gray">
-              X ITEMS
+            <Heading as="h4" color="brand.gray" size="subtitle" textTransform="uppercase">
+              {/* TO DO: update this to be the length of the line items in cart */}
+              {`${products.length} ITEMS`}
             </Heading>
           </Flex>
           <Divider />
-          {/* TO DO: change to 36px in between the pictures */}
-          {/* The products here should be the line-items in the Shopify Checkout */}
-          {/* Will need to specify a key here */}
-
-          {/* Need to consider if the quantity picker should be inside the CartItem component */}
+          {/* TO DO: The products here should be the line-items in the Shopify Checkout, also need to add unique key */}
           {products &&
             products.map(product => (
               <Box w="100%" py="10px">
@@ -73,7 +69,6 @@ const Cart = () => {
                 type="coupon"
                 name="coupon"
                 placeholder="123ABCD"
-                // Need to change on-hover colour
                 // onChange={e => handleInputChange(e, i)}
               />
             </FormControl>
@@ -83,11 +78,8 @@ const Cart = () => {
           </HStack>
         </VStack>
         <VStack alignItems="flex-start" pt="92px">
-          {/* Fix the 92px, it's super sketch */}
-          <VStack alignItems="flex-start" bg="#E8E8E8" spacing="40px" px="36px" py="36px" w="409px">
-            {/* Change this 409px */}
-            {/* Current the values in Order Summary are just placeholders */}
-            {/* Probably want to extract the order summary to another component */}
+          <VStack alignItems="flex-start" bg="brand.lightgray" spacing="40px" p="36px" w="409px" mb="24px">
+            {/* The current values in Order Summary are just placeholders */}
             <Heading as="h4" size="subtitle" color="brand.black">
               ORDER SUMMARY
             </Heading>
