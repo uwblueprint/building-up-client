c442db08e7a68eee33ba306f2c644149ded03e44
Tony Zhao <tawnyzhao@gmail.com>
2021-04-11 12:38:56 -0700

    Add mailbox image to email verification

--- src/pages/EmailVerification/EmailVerificationSent.js
+++ src/pages/EmailVerification/EmailVerificationSent.js
@@ -5,6 +5,7 @@ import { Box, Flex, Text, Image, Button } from '@chakra-ui/react';
 
 import { SEND_VERIFICATION_EMAIL } from 'data/gql/user';
 import logo from 'assets/images/logo-black.png';
+import mailbox from 'assets/images/mailbox.png';
 
 const EmailVerificationSent = props => {
   const {
@@ -28,7 +29,7 @@ const EmailVerificationSent = props => {
 
   return (
     <Box w="100%" justify="center" align="center">
-      <Flex justify="center" mt="54px">
+      <Flex justify="center">
         <Image src={logo} w="350px" />
       </Flex>
       <Text fontSize="3xl" height="52px" mt="42px">
@@ -38,13 +39,13 @@ const EmailVerificationSent = props => {
         You will need to verify your email to access your dashboard.
       </Text>
       <Flex justify="center" mt="35px">
-        <Image src={logo} h="250px" w="250px" />
+        <Image src={mailbox} h="250px" w="250px" />
       </Flex>
       <Text fontSize="xl" w="50%" line-height="35px" mt="33px">
         An email has been sent to {email} with a link to verify your account. If you have not received the email after a
         few minutes, please check your spam folder.
       </Text>
-      <Button size="lg" _focus={{ boxShadow: 'white' }} mt="43px" onClick={handleResendEmail}>
+      <Button size="lg" _focus={{ boxShadow: 'white' }} my="43px" onClick={handleResendEmail}>
         Resend Email
       </Button>
     </Box>
