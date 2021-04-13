efb34e252189012596245382cd72fb057cabce67
Jeffrey Huang <jeff.huang62@gmail.com>
2021-04-13 19:49:27 -0400

    Refactor email verification screens to be more declarative * Also abstract the black button into Chakra component styling

--- src/pages/EmailVerification/EmailVerificationSent.js
+++ src/pages/EmailVerification/EmailVerificationSent.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { useSelector } from 'react-redux';
-import { Box, Flex, Text, Image, Button, Heading } from '@chakra-ui/react';
+import { Flex, Text, Image, Button, Heading, useToast } from '@chakra-ui/react';
 
 import { SEND_VERIFICATION_EMAIL } from 'data/gql/user';
+import Toast from 'components/dashboard/Toast/Toast';
 import logo from 'assets/images/logo-black.png';
 import mailbox from 'assets/images/mailbox.png';
 
@@ -12,51 +13,54 @@ const EmailVerificationSent = props => {
     user: { userId, email },
   } = useSelector(state => state.auth);
 
-  const [sendVerificationEmail] = useMutation(SEND_VERIFICATION_EMAIL);
+  const toast = useToast();
+  const [sendVerificationEmail, { loading, data, error }] = useMutation(SEND_VERIFICATION_EMAIL);
 
   const handleResendEmail = e => {
     sendVerificationEmail({
       variables: { id: userId },
-    })
-      .then(data => {
-        console.log(data);
-        // Create a toast or alert to indicate emails have been sent
-      })
-      .catch(e => {
-        console.log(e);
-      });
+    });
   };
 
+  useEffect(() => {
+    if (error) {
+      toast({
+        title: 'Error sending email confirmation email, please try again.',
+        status: 'error',
+        isClosable: true,
+      });
+    }
+  }, [error, toast]);
+
+  useEffect(() => {
+    if (data) {
+      toast({
+        position: 'top',
+        render: props => (
+          <Toast {...props} description="Verification email sent again, please check your email." isClosable />
+        ),
+      });
+    }
+  }, [data, toast]);
+
   return (
-    <Box w="100%" justify="center" align="center">
-      <Flex justify="center">
-        <Image src={logo} w="350px" />
-      </Flex>
+    <Flex direction="column" h="100%" w="100%" align="center" textAlign="center">
+      <Image src={logo} w="350px" />
       <Heading size="h1" as="h1" mt="42px">
         Verify your email
       </Heading>
       <Text fontSize="xl" w="50%" line-height="35px" mt="16px">
         You will need to verify your email to access your dashboard.
       </Text>
-      <Flex justify="center" mt="35px">
-        <Image src={mailbox} h="250px" w="250px" />
-      </Flex>
+      <Image src={mailbox} h="250px" w="250px" mt="35px" />
       <Text fontSize="xl" w="50%" line-height="35px" mt="33px">
         An email has been sent to {email} with a link to verify your account. If you have not received the email after a
         few minutes, please check your spam folder.
       </Text>
-      <Button
-        sice="lg"
-        textColor="white"
-        bg="black"
-        _hover={{ bg: 'gray.800' }}
-        _active={{ bg: 'gray.700' }}
-        my="43px"
-        onClick={handleResendEmail}
-      >
+      <Button size="lg" variant="black" my="43px" isLoading={loading} onClick={handleResendEmail}>
         Resend Email
       </Button>
-    </Box>
+    </Flex>
   );
 };
 
--- src/pages/EmailVerify/index.js
+++ src/pages/EmailVerify/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, NavLink } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useParams, Link as RouterLink } from 'react-router-dom';
 import { VERIFY_ACCOUNT } from 'data/gql/user';
 import { useMutation } from '@apollo/client';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,13 +7,12 @@ import { Box, Center, Heading, Spinner, Text, Flex, Image, Button } from '@chakr
 import { UPDATE_USER_VERIFICATION } from 'data/actions/type';
 import logo from 'assets/images/logo-black.png';
 
-function EmailVerify(props) {
+function EmailVerify() {
   const dispatch = useDispatch();
   const { hash } = useParams();
   const {
     user: { userId: id },
   } = useSelector(state => state.auth);
-  const [isValidLink, setValidLink] = useState(false);
 
   const [verifyAccount, { loading: verifyAccountLoading, data: verifyAccountData }] = useMutation(VERIFY_ACCOUNT);
 
@@ -29,14 +28,11 @@ function EmailVerify(props) {
         verifyAccount: { isVerified },
       } = verifyAccountData;
       dispatch({ type: UPDATE_USER_VERIFICATION, payload: isVerified });
-      if (isVerified) {
-        setValidLink(true);
-      }
     }
   }, [dispatch, verifyAccountData]);
 
-  return verifyAccountLoading ? (
-    <Center h="100%">
+  return verifyAccountLoading || !verifyAccountData ? (
+    <Center w="100%" h="100%">
       <Spinner size="xl" />
     </Center>
   ) : (
@@ -46,24 +42,15 @@ function EmailVerify(props) {
       </Flex>
       <Box>
         <Heading size="h1" as="h1" mt="75px">
-          {isValidLink ? 'Email Verified' : "Oops! The link you've followed is invalid."}
+          {verifyAccountData.verifyAccount.isVerified ? 'Email Verified' : "Oops! The link you've followed is invalid."}
         </Heading>
       </Box>
       <Text fontSize="2xl" mt="32px">
-        {isValidLink
+        {verifyAccountData.verifyAccount.isVerified
           ? 'Your email has been verified successfully.'
           : 'Please check your email for the correct verification link.'}
       </Text>
-      <Button
-        sice="lg"
-        textColor="white"
-        bg="black"
-        _hover={{ bg: 'gray.800' }}
-        _active={{ bg: 'gray.700' }}
-        my="46px"
-        as={NavLink}
-        to="/home"
-      >
+      <Button size="lg" variant="black" my="46px" as={RouterLink} to="/home">
         Return to Home
       </Button>
     </Box>
