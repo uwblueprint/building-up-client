5840a831077c01f20b6809501dbea3a9a534f890
Jeffrey Huang <jeff.huang62@gmail.com>
2021-04-18 13:59:09 -0400

    Responsive design for product page * Also disable adding to cart if out of stock

--- src/components/storefront/ProductDetails/ProductDetails.js
+++ src/components/storefront/ProductDetails/ProductDetails.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Heading, Flex, Image, Text, Button, useNumberInput } from '@chakra-ui/react';
+import { Heading, Flex, Image, Text, Button, Stack, VStack, useNumberInput } from '@chakra-ui/react';
 import AdditionalContent from './AdditionalContent';
 import QuantityPicker from './QuantityPicker';
 import { useShopify } from 'hooks/useShopify';
@@ -7,7 +7,7 @@ import { useHistory } from 'react-router';
 
 const ProductDetails = ({ product, checkout }) => {
   const { id: checkoutId } = checkout;
-  const { images, title, description, variants } = product;
+  const { images, title, description, variants, availableForSale } = product;
   const { id: variantId, sku: variantSku, price: variantPrice } = variants[0];
   const { addLineItems } = useShopify();
   const history = useHistory();
@@ -33,13 +33,13 @@ const ProductDetails = ({ product, checkout }) => {
   });
 
   return (
-    <Flex>
-      <Flex direction="column" w="100%" h="100%">
+    <Stack direction={{ base: 'column', sm: 'row' }} spacing={8}>
+      <VStack spacing={6} flex={1} h="100%">
         {images.map(({ id, src }) => (
-          <Image key={id} boxSize="100%" border="1px solid black" src={src} alt={title} mb={6} />
+          <Image key={id} boxSize="100%" border="1px solid black" src={src} alt={title} />
         ))}
-      </Flex>
-      <Flex direction="column" w="100%" pl={10}>
+      </VStack>
+      <Flex direction="column" flex={1}>
         <Heading as="h4" size="subtitle" textTransform="uppercase" color="brand.gray">
           {`Item #${variantSku}`}
         </Heading>
@@ -50,13 +50,26 @@ const ProductDetails = ({ product, checkout }) => {
           {`$${variantPrice}`}
         </Heading>
         <Text my={5}>{description}</Text>
-        <QuantityPicker {...quantityPickerProps} />
-        <Button textTransform="uppercase" mt={5} mb={10} onClick={handleAddToShoppingBag}>
-          {'Add to shopping bag'}
+        {availableForSale ? (
+          <QuantityPicker {...quantityPickerProps} />
+        ) : (
+          <Heading as="h3" size="h3" textTransform="uppercase">
+            Out of Stock
+          </Heading>
+        )}
+        <Button
+          textTransform="uppercase"
+          mt={5}
+          mb={10}
+          minW="auto"
+          onClick={handleAddToShoppingBag}
+          disabled={!availableForSale}
+        >
+          Add to shopping bag
         </Button>
         <AdditionalContent />
       </Flex>
-    </Flex>
+    </Stack>
   );
 };
 
