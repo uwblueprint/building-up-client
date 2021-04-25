9aca9fad76ba1b1860c4292e2f942ada36e2a351
Jeffrey Huang <jeff.huang62@gmail.com>
2021-04-25 13:35:53 -0400

    Unsticky the navbar subheading, and make it responsive

--- src/components/storefront/TeamBanner/TeamBanner.js
+++ src/components/storefront/TeamBanner/TeamBanner.js
@@ -2,6 +2,10 @@ import { Flex, Spinner, Text, useDisclosure } from '@chakra-ui/react';
 import { InfoOutlineIcon } from '@chakra-ui/icons';
 import NoTeamAssociationModal from '../NoTeamAssociationModal/NoTeamAssociationModal';
 
+import { PageContainer } from 'components/storefront/PageContainer/PageContainer';
+
+const TEAM_BANNER_HEIGHT = 34;
+
 const TeamBanner = props => {
   const { loading, error, data } = props;
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -11,30 +15,29 @@ const TeamBanner = props => {
       onClick={!loading && (error || !data) ? onOpen : null}
       cursor={!loading && (error || !data) ? 'pointer' : null}
       w="100%"
-      h="34px"
-      px={3}
-      justify="center"
-      align="center"
+      h={`${TEAM_BANNER_HEIGHT}px`}
       bg={loading || data ? 'brand.lightgray' : 'brand.red'}
     >
-      {loading ? (
-        <Spinner size="sm" />
-      ) : (
-        <>
-          <Text
-            color={loading || data ? 'black' : 'white'}
-            textTransform="uppercase"
-            fontWeight="bold"
-            fontFamily="heading"
-            fontSize="sm"
-            isTruncated
-          >
-            {data ? `Team ${data.getTeam.name}` : 'Please note that your purchase will not be attributed to a team'}
-          </Text>
-          {(error || !data) && <InfoOutlineIcon color="white" marginLeft="8px" />}
-        </>
-      )}
-      <NoTeamAssociationModal isOpen={isOpen} onClose={onClose} />
+      <PageContainer justifyContent="center" alignItems="center" py={0} flexDirection="row">
+        {loading ? (
+          <Spinner size="sm" />
+        ) : (
+          <>
+            <Text
+              color={loading || data ? 'black' : 'white'}
+              textTransform="uppercase"
+              fontWeight="bold"
+              fontFamily="heading"
+              fontSize="sm"
+              isTruncated
+            >
+              {data ? `Team ${data.getTeam.name}` : 'Please note that your purchase will not be attributed to a team'}
+            </Text>
+            {(error || !data) && <InfoOutlineIcon color="white" marginLeft="8px" />}
+          </>
+        )}
+        <NoTeamAssociationModal isOpen={isOpen} onClose={onClose} />
+      </PageContainer>
     </Flex>
   );
 };
